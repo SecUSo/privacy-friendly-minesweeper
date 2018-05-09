bc482f584df7539f3b6c6e7474b6f6d89d9f0223
Max Dreger <dreger.max@web.de>
2018-05-09 15:33:59 +0200

    Finalised PlayingField Layout. Made cells square and PlayingField always fit the screen

--- app/src/main/java/org/secuso/privacyfriendlyminesweeper/activities/PlayActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlyminesweeper/activities/PlayActivity.java
@@ -83,7 +83,7 @@ public class PlayActivity extends BaseActivity implements PlayRecyclerViewAdapte
         numberOfRows = rows;
 
         //TODO: create playing field of buttons from parameters (so far only testing from this point onwards)
-        //TODO: fix positions when screen is being flipped, make fields change size depending on amount in column/row
+        //TODO: fix positions when screen is being flipped
         // data to populate the RecyclerView with
         String[] data = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"};
 
