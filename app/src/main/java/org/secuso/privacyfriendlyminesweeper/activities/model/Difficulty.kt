4688acb4b1e4e0ec74e7c26cad5ef9a3cbdc938f
Patrick Schneider <patrick.schneider@student.kit.edu>
2022-12-26 13:29:28 +0100

    Added missing license

--- app/src/main/java/org/secuso/privacyfriendlyminesweeper/activities/model/Difficulty.kt
+++ app/src/main/java/org/secuso/privacyfriendlyminesweeper/activities/model/Difficulty.kt
@@ -1,3 +1,19 @@
+/*
+ This file is part of Privacy Friendly Minesweeper.
+
+ Privacy Friendly Minesweeper is free software:
+ you can redistribute it and/or modify it under the terms of the
+ GNU General Public License as published by the Free Software Foundation,
+ either version 3 of the License, or any later version.
+
+ Privacy Friendly Minesweeper is distributed in the hope
+ that it will be useful, but WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Privacy Friendly Minesweeper. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.secuso.privacyfriendlyminesweeper.activities.model
 
 class Difficulty(val type: Type, val rows: Int, val cols: Int, val bombs: Int) {
